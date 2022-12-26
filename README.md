107cb93ca86a0766b52996f9910be841b716994d
minhbd <minhbd@pascaliaasia.com>
2022-12-26 09:44:51 +0700

    [MOD] minor changes

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-### How to use
+## sonar-runner-for-OZV
+This is a script that can be used to run a SonarQube analysis on modified files in a Git repository. It is designed to be used with the OZV project.
 
-#### First time setup
+### Prerequisites
+Before running the script, you will need to configure the following variables:
+
+```
+SONAR_URL: The URL of the SonarQube server.
+SONAR_TOKEN: The SonarQube token used to authenticate the analysis.
+GITHUB_REPOSITORY: The name of the GitHub repository.
+SONAR_PROJECT_PROPERTIES: The path to the SonarQube project properties file.
+GIT_EXECUTABLE: The path to the Git executable.
+```
+
+### First setup
 1. Run `make main`
 2. Access http://localhost:9000
 3. Change admin password (Default: admin/admin)
@@ -8,8 +20,8 @@
 5. Set SONAR_TOKEN in `run.sh` file after Sonar generated token from admin page.
 6. Run `make start`
 
-#### Run
-1. Run `make start`
-
 ### Note
 - Make sure base branch and compared branch are up to date.
+
+### License
+This project is licensed under the MIT License - see the LICENSE file for details.
--- run.sh
+++ run.sh
@@ -1,61 +1,12 @@
-SOURCE_FOLDER=""
+#!/bin/bash
 
-# TODO: Change configuration here
-PS3='Please enter your choice: '
-options=("mkproduction" "aslan" "maple" "apollo" "woodstock" "spitz" "app-server-side")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "aslan")
-            echo "you chose choice $REPLY which is $opt"
-            SOURCE_FOLDER="/Users/minh/Hapitas/aslan"
-            break;
-            ;;
-        "maple")
-            echo "you chose choice $REPLY which is $opt"
-            SOURCE_FOLDER="/Users/minh/Hapitas/aslan"
-            break;
-            ;;
-        "apollo")
-            echo "you chose choice $REPLY which is $opt"
-            SOURCE_FOLDER="/Users/minh/Hapitas/apollo"
-            break;
-            ;;
-        "woodstock")
-            echo "you chose choice $REPLY which is $opt"
-            SOURCE_FOLDER="/Users/minh/Hapitas/woodstock"
-            break;
-            ;;
-        "spitz")
-            echo "you chose choice $REPLY which is $opt"
-            SOURCE_FOLDER="/Users/minh/Hapitas/spitz"
-            break;
-            ;;
-        "app-server-side")
-            echo "you chose choice $REPLY which is $opt"
-            SOURCE_FOLDER="/Users/minh/Hapitas/app-server-side"
-            break;
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
-
-echo "What base branch?"
-read BASE_BRANCH_NAME
-
-# Set the branch and commit information
-echo "What branch to compare?"
-read BRANCH_NAME
+# Set the base branch and commit information
+BASE_BRANCH_NAME="master"
 
 # Set the SonarQube server URL and token
-# TODO: Change configuration here
 SONAR_URL="http://localhost:9000"
-# TODO: Change configuration here
 SONAR_TOKEN="sqp_0fa8561c119afbd544833c77c4deef3b2e3b39e6"
-# TODO: Change configuration here
+
 # Set the GitHub repository information
 GITHUB_REPOSITORY="test-project"
 
@@ -65,19 +16,60 @@ SONAR_PROJECT_PROPERTIES="sonar-project.properties"
 # Set the location of the Git executable
 GIT_EXECUTABLE="git"
 
+# Set the list of options and corresponding project paths
+options=(
+    "aslan"
+    "maple"
+    "apollo"
+    "woodstock"
+    "spitz"
+    "app-server-side"
+)
+project_paths=(
+    "/Users/minh/Hapitas/aslan"
+    "/Users/minh/Hapitas/aslan"
+    "/Users/minh/Hapitas/apollo"
+    "/Users/minh/Hapitas/woodstock"
+    "/Users/minh/Hapitas/spitz"
+    "/Users/minh/Hapitas/app-server-side"
+)
+
+# Prompt the user to select a project
+PS3='Please enter your choice: '
+select opt in "${options[@]}"
+do
+    if [[ "$REPLY" -le ${#options[@]} ]]; then
+        SOURCE_FOLDER=${project_paths[$((REPLY-1))]}
+        break
+    else
+        echo "Invalid option $REPLY"
+    fi
+done
+
+# Prompt the user to enter the branch to compare
+echo "What base branch?"
+read BASE_BRANCH_NAME
+
+# Prompt the user to enter the branch to compare
+echo "What branch to compare?"
+read BRANCH_NAME
+
+# Set the current path and create a file_changes directory
 CURRENT_PATH=$PWD
 rm -rf file_changes && mkdir file_changes
 
-# Get the list of modified files in the current branch compared to the master branch
-cd $SOURCE_FOLDER
-for name in $($GIT_EXECUTABLE diff --name-only $BASE_BRANCH_NAME..$BRANCH_NAME); do cp $name $CURRENT_PATH/file_changes ; done
-cd $CURRENT_PATH
-MODIFIED_FILES=$(ls -d $CURRENT_PATH/file_changes/* | tr '\n' ',')
+# Change to the project directory and get the list of modified files
+cd "$SOURCE_FOLDER"
+MODIFIED_FILES=$($GIT_EXECUTABLE diff --name-only "$BASE_BRANCH_NAME".."$BRANCH_NAME" | xargs -I{} cp {} "$CURRENT_PATH/file_changes")
+
+# Return to the current path and set the modified files list
+cd "$CURRENT_PATH"
+MODIFIED_FILES=$(ls -d "$CURRENT_PATH/file_changes"/* | tr '\n' ',')
 
 # Set the SonarQube project properties
-echo "sonar.projectKey=${GITHUB_REPOSITORY}" > $SONAR_PROJECT_PROPERTIES
-echo "sonar.host.url=${SONAR_URL}" >> $SONAR_PROJECT_PROPERTIES
-echo "sonar.login=${SONAR_TOKEN}" >> $SONAR_PROJECT_PROPERTIES
+echo "sonar.projectKey=${GITHUB_REPOSITORY}" > "$SONAR_PROJECT_PROPERTIES"
+echo "sonar.host.url=${SONAR_URL}" >> "$SONAR_PROJECT_PROPERTIES"
+echo "sonar.login=${SONAR_TOKEN}" >> "$SONAR_PROJECT_PROPERTIES"
 echo "sonar.sources=${MODIFIED_FILES}" >> $SONAR_PROJECT_PROPERTIES
 echo "sonar.scm.exclusions.disabled=true" >> $SONAR_PROJECT_PROPERTIES
 
